70eb3bef7b0265f46980836bb7a410b6018bf015
Ashraz Rashid <arashid@digitalauxilius.com>
2024-06-14 10:14:57 +0500

    project run successfully

--- android/app/src/main/java/com/arexample/MainActivity.kt
+++ android/app/src/main/java/com/arexample/MainActivity.kt
@@ -4,9 +4,20 @@ import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import com.unity3d.player.UnityPlayerActivity;
+import android.content.Intent
+import android.os.Bundle
+
 
 class MainActivity : ReactActivity() {
 
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Initialize Unity
+        val intent = Intent(this, UnityPlayerActivity::class.java)
+        startActivity(intent)
+    }
     /**
      * Returns the name of the main component registered from JavaScript. This is used to schedule
      * rendering of the component.
